80a66b865c0ea8cfa689eed03eb4d05480bc0ba3
Nathaniel Remy <nathanielremy1@gmail.com>
2021-10-16 18:11:47 -0300

    gang plan popup for Suicide Gang

--- TrueNFTs/Public/js/drop.js
+++ TrueNFTs/Public/js/drop.js
@@ -1,10 +1,11 @@
+//Show Lightbox of NFT when NFT clicked
 const lightBox = document.createElement('div');
-lightBox.id = 'lightBox';
-document.body.appendChild(lightBox);
 const lightBoxItems = document.getElementsByClassName('lightBoxItem');
 for (var item = 0; item < lightBoxItems.length; item++) {
   const selectedItem = lightBoxItems[item];
   selectedItem.addEventListener('click', (e) => {
+    lightBox.id = 'lightBox';
+    document.body.appendChild(lightBox);
     lightBox.classList.add('active');
     const lightBoxImage = document.createElement('img');
     lightBoxImage.src = selectedItem.src;
@@ -22,4 +23,18 @@ lightBox.addEventListener('click', e => {
     return;
   };
   lightBox.classList.remove('active');
+});
+
+//Show Suicide Gang roadmap when button clicked
+const gangPlanLB = document.querySelector('.gangPlanLB');
+gangPlanLB.addEventListener('click', e => {
+  if (e.target !== e.currentTarget) {
+    return;
+  };
+  gangPlanLB.classList.remove('active');
+});
+
+const gangPlan = document.querySelector('.gangPlanButton');
+gangPlan.addEventListener('click', e => {
+  gangPlanLB.classList.add('active');
 });
